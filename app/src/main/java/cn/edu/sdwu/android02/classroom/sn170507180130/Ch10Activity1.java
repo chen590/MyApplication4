fb3284482a6b6e8fcce00fc83af370bcc647befb
chen590 <281365931@qq.com>
2020-04-22 14:36:57 +0800

    reason

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180130/Ch10Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180130/Ch10Activity1.java
@@ -1,23 +1,30 @@
 package cn.edu.sdwu.android02.classroom.sn170507180130;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class Ch10Activity1 extends AppCompatActivity {
-
+private Integer count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         Log.i(Ch10Activity1.class.toString(),"onCreate");
         setContentView(R.layout.layout_ch10_1);
+        count=0;
+        //接收数据
+        Intent intent=getIntent();//获取界面跳转时使用的intent
+        String text=intent.getStringExtra("text");
+        TextView textView=(TextView)findViewById(R.id.ch10_1_tv);
+        textView.setText(text);
     }
 
     public void finishClick(View view){
         finish();//关闭界面
-
     }
 
     @Override
